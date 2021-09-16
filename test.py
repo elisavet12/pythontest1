f0891e7d972f6e76de10fc8fc953129382c659f8
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2021-09-16 20:35:01 +0300

    python code update

---
+++ test.py
@@ -0,0 +1,24 @@
+str='ballfromtheotherside'
+print("Length of str=", len(str))
+
+str='ballfromtheotherside'
+print(str[3])
+
+s="bananabananabanana"
+print("First position of the letter a = ", str.index('a'))
+
+
+s="bananabananabanana"
+print("It has ", s.count("a"))
+
+s="bananabananabanana"
+for s in "bananabananabanana":print(s)
+
+str="HeLLo, hoW aRe You?"
+str=str.lower()
+print(str)
+str=str.upper()
+print(str)
+
+
+
